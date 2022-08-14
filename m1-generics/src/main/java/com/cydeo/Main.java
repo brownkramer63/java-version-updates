8af9898164ffef2fa38ad3df700fd92d4c0a9e1e
Kramer Brown <brownkramer63@gmail.com>
2022-08-14 10:56:10 -0500

    Generics

--- m1-generics/src/main/java/com/cydeo/Main.java
+++ m1-generics/src/main/java/com/cydeo/Main.java
@@ -1,9 +1,27 @@
 package com.cydeo;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        ArrayList<Integer> items = new ArrayList(); //ArrayList only accepts objects belongs to Integer claass
 
+        items.add(1);
+        items.add(2);
+        items.add(3);
+        items.add(4);
+//        items.add("Apple");
+
+        printDoubled(items);
+
+    }
+
+    private static void printDoubled(ArrayList<Integer> items) {
+        for (int each: items){
+            System.out.println(each*2);
+
+        }
     }
 }
